0b6f7f4425371b707458f1ec27b62ddc3901078c
totarochristian <totaro.christian.18@gmail.com>
2023-03-22 16:33:48 +0100

    Define main goals, milestones,, bonus and tips in the script file (as comment)

---
+++ js/script.js
@@ -0,0 +1,38 @@
+/*
+------------------------------------------------------------------------------
+-------------------------------- Main goals ----------------------------------
+------------------------------------------------------------------------------
+Using the data provided, create an array of objects to represent team 
+members.
+Each member is characterized by the following information: name, role and 
+photo.
+------------------------------------------------------------------------------
+-------------------------------- Milestones ----------------------------------
+------------------------------------------------------------------------------
+MILESTONE 0:
+Create the array of objects with the information provided.
+
+MILESTONE 1:
+Print the name, role and photo string information for each team member on the 
+console
+
+MILESTONE 2:
+Print the same information to DOM as strings
+------------------------------------------------------------------------------
+---------------------------------- Bonus -------------------------------------
+------------------------------------------------------------------------------
+BONUS 1:
+Transform the photo string into an actual image
+BONUS 2:
+Organize individual members into cards/sheets
+------------------------------------------------------------------------------
+----------------------------------- Tips -------------------------------------
+------------------------------------------------------------------------------
+Tips of the day:
+- Let's think step by step as always.
+- First the logic in Italian and then we translate into code.
+- And remember that console.log() is our friend!
+------------------------------------------------------------------------------
+------------------------------------------------------------------------------
+------------------------------------------------------------------------------
+*/
